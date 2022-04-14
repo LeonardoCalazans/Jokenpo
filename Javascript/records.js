1b0bbf9ff40a94fb9b0dc56b88b42c987e76648a
Leonardo Pinto Silva Calazans <leonardopintosilva@hotmail.com>
2022-04-13 22:49:05 -0300

    feat(firebase-get): adding firebase  get module in record list and refactor name Records for displayingRecords

--- Javascript/records.js
+++ Javascript/records.js
@@ -1,11 +1,14 @@
 import darkMode from './module/DarkMode/darkMode.js';
 import initDropdownMenu from './module/NavbarMenu/dropdownMenu.js';
 import initMenu from './module/NavbarMenu/menu.js';
-import initGetRecords from './module/Records/getRecords.js';
+import { displayingRecords } from './module/Records/displayingRecords.js';
 import onClickOrderRecords from './module/Records/orderRecords.js';
+import { initFirebase } from './module/Firestore/initFirebase.js';
+export const recordUl = document.querySelector('#recordList');
 
+initFirebase();
 initDropdownMenu();
 initMenu();
-initGetRecords();
+displayingRecords(recordUl);
 darkMode();
 onClickOrderRecords();
