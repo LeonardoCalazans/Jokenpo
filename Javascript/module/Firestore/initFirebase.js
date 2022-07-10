9f96ee5a6ed4179f732b405096c34d35066786a5
Leonardo <60453161+LeonardoCalazans@users.noreply.github.com>
2022-07-10 01:26:27 -0300

    chore: Update init Firebase

--- Javascript/module/Firestore/initFirebase.js
+++ Javascript/module/Firestore/initFirebase.js
@@ -6,14 +6,14 @@ import { query, orderBy, limit, where, onSnapshot } from "https://www.gstatic.co
 
 export function initFirebase() {
     const firebaseConfig = {
-        apiKey: "AIzaSyA55ZXbJqMAtcx8FJvqb7JdZjUFa2KCwiw",
-        authDomain: "jokenpo-5661a.firebaseapp.com",
-        databaseURL: "https://jokenpo-5661a-default-rtdb.firebaseio.com",
-        projectId: "jokenpo-5661a",
-        storageBucket: "jokenpo-5661a.appspot.com",
-        messagingSenderId: "1076014675228",
-        appId: "1:1076014675228:web:878ff073e8fc418a5a76d9",
-        measurementId: "G-Q0L37559HT"
+        apiKey: process.env.FIREBASE_API_KEY,
+        authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+        databaseURL: process.env.FIREBASE_DATABASE_URL,
+        projectId: process.env.FIREBASE_PROJECT_ID,
+        storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+        messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+        appId: process.env.FIREBASE_APP_ID,
+        measurementId: process.env.FIREBASE_MEASUREMENT_ID"
     };
 
     return firebaseConfig
